5098477f9de8786df343133640467150f50844b2
Finn Lambert Home PC <107528359+FinnLambert465@users.noreply.github.com>
2023-09-24 18:25:54 +1000

    Added assets and made character

--- Source/StealthFPS/StealthPlayerCharacter.cpp
+++ Source/StealthFPS/StealthPlayerCharacter.cpp
@@ -2,6 +2,9 @@
 
 
 #include "StealthPlayerCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
 
 // Sets default values
 AStealthPlayerCharacter::AStealthPlayerCharacter()
@@ -9,6 +12,24 @@ AStealthPlayerCharacter::AStealthPlayerCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	GetCapsuleComponent()->InitCapsuleSize(40, 95);
+
+	turnHorizontalRate = 45;
+	turnVerticalRate = 45;
+
+	firstPersonCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("First Person Camera"));
+	firstPersonCamera->SetupAttachment(GetCapsuleComponent());
+	firstPersonCamera->AddRelativeLocation(FVector(-39.65f, 1.75f, 64));
+	firstPersonCamera->bUsePawnControlRotation = true;
+
+	bodyMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Character Mesh"));
+
+	bodyMesh->SetOnlyOwnerSee(true);
+	bodyMesh->SetupAttachment(firstPersonCamera);
+	bodyMesh->bCastDynamicShadow = false;
+	bodyMesh->CastShadow = false;
+	bodyMesh->AddRelativeRotation(FRotator(1.9f, -19.19f, 5.2f));
+	bodyMesh->AddRelativeLocation(FVector(-0.5f, -4.4f, -155.7f));
 }
 
 // Called when the game starts or when spawned
@@ -36,14 +57,26 @@ void AStealthPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerI
 	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, &AStealthPlayerCharacter::Jump);
 }
 
-void AStealthPlayerCharacter::MoveForward(float AxisValue)
+void AStealthPlayerCharacter::FireGun()
 {
-	AddMovementInput(GetActorForwardVector() * AxisValue);
 }
 
-void AStealthPlayerCharacter::MoveRight(float AxisValue)
+void AStealthPlayerCharacter::MoveForward(float axisValue)
 {
-	AddMovementInput(GetActorRightVector() * AxisValue);
+	AddMovementInput(GetActorForwardVector() * axisValue);
+}
+
+void AStealthPlayerCharacter::MoveRight(float axisValue)
+{
+	AddMovementInput(GetActorRightVector() * axisValue);
+}
+
+void AStealthPlayerCharacter::VerticalTurnAtRate(float rate)
+{
+}
+
+void AStealthPlayerCharacter::HorizontalTurnAtRate(float rate)
+{
 }
 
 
--- Source/StealthFPS/StealthPlayerCharacter.h
+++ Source/StealthFPS/StealthPlayerCharacter.h
@@ -26,9 +26,35 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;\
 
+public:
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USkeletalMeshComponent* bodyMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USkeletalMeshComponent* gunMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USceneComponent* muzzleLocation;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = camera)
+		class UCameraComponent* firstPersonCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = camera)
+		float turnVerticalRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = camera)
+		float turnHorizontalRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		FVector gunOffset;
 
 protected:
-	void MoveForward(float AxisValue);
-	void MoveRight(float AxisValue);
+
+	void FireGun();
+
+	void MoveForward(float axisValue);
+	void MoveRight(float axisValue);
+	void VerticalTurnAtRate(float rate);
+	void HorizontalTurnAtRate(float rate);
 
 };
